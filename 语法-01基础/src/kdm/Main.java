35e2e84f2e9c8ace37b45e777c7f7f4e1beea1d9
xionglp <785874752@qq.com>
2020-12-11 10:25:53 +0800

    提交代码

--- 语法-01基础/src/kdm/Main.java
+++ 语法-01基础/src/kdm/Main.java
@@ -3,15 +3,7 @@ package kdm;
 public class Main {
 
 	public static void main(String[] args) {
-
 		System.out.println("hello Java");
-		System.out.println("好好学习， 坚持、专注");
-
-		System.out.println("hello world");
-		System.out.println("hello world");
-		System.out.println("hello world");
-		System.out.println("hello world");
-
 	}
 
 }
